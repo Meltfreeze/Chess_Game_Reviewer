6fd309bcc05bec96aba70735f918b6a5b7124d2f
Gaurav Kanaujiya <gaurav5504.gk@gmail.com>
2026-09-02 21:46:30 +0530

    Inaccuracy fixed

--- backend/main.py
+++ backend/main.py
@@ -174,7 +174,7 @@ def icon(name: str):
         "Excellent": "Excellent.png",
         "Good": "Good.png",
         "Book": "Book.png",
-        "Inaccuracy": "Inacuracy.png",
+        "Inaccuracy": "Inaccuracy.png",
         "Miss": "Miss.png",
         "Mistake": "Mistake.png",
         "Blunder": "Blunder.png",
